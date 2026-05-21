aa5a3c3b326f28ce817e182cb7739579bb3e8d62
Zarif <roundspecs@pm.me>
2026-05-21 19:42:59 +0600

    feat: add validation check to add command

--- cmd/add.go
+++ cmd/add.go
@@ -1,7 +1,20 @@
 package cmd
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
 func Add(args []string) {
-	fmt.Println(args)
+	if len(args) == 0 {
+		fmt.Println("Error: task description is required")
+		fmt.Println("Usage: task add \"buy groceries\"")
+		fmt.Println("   or, task add buy groceries")
+		os.Exit(1)
+	}
+
+	description := strings.Join(args, " ")
+
+	fmt.Println("Description:", description)
 }
